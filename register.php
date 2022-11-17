e3d21e4ec33146c95f6e98aa026a9d162eaa1732
Doanzedrow <Tranhuudoan2003@gmail.com>
2022-11-18 00:14:50 +0700

    nhom8

---
+++ register.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Tạo Trang Login</title>
+     <link type="text/css" rel="stylesheet" href="css/stylelogin.css"/>
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<section>
+     <!--Bắt Đầu Phần Hình Ảnh-->
+     <div class="img-bg">
+         <img src="https://niemvuilaptrinh.ams3.cdn.digitaloceanspaces.com/tao_trang_dang_nhap/hinh_anh_minh_hoa.jpg" alt="Hình Ảnh Minh Họa">
+     </div>
+     <!--Kết Thúc Phần Hình Ảnh-->
+     <!--Bắt Đầu Phần Nội Dung-->
+     <div class="noi-dung">
+         <div class="form">
+             <h2>Trang Đăng Ký</h2>
+             <form action="login.php">
+                 <div class="input-form">
+                     <span>Tên Người Dùng</span>
+                     <input type="text" name="">
+                 </div>
+                 <div class="input-form">
+                     <span>Mật Khẩu</span>
+                     <input type="password" name="">
+                 </div>
+                 <div class="input-form">
+                     <span>Nhập Lại Mật Khẩu</span>
+                     <input type="password" name="">
+                 </div>              
+                 <div class="input-form">
+                     <input type="submit" value="Đăng Ký">
+                 </div>              
+             </form>           
+         </div>
+     </div>
+     <!--Kết Thúc Phần Nội Dung-->
+ </section>
+</body>
+</html>
